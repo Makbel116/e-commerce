6f7ad5afa4574199ef57adcc49b6e819a4131229
Eyuel Debrework <makbel9395@gmail.com>
2024-03-12 05:03:21 -0700

    created and run migration called 'create_items_table'

---
+++ database/migrations/2024_03_12_081125_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price'); 
+            $table->integer('rating');
+            $table->integer('amount');
+            $table->decimal('discount')->default(0);   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_item');
+    }
+}
